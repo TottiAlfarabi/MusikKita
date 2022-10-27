de9b41f81543b7022d731596cdb02edefeed7af3
Billyfer <114042064+Billyfer@users.noreply.github.com>
2022-10-27 15:30:58 +0700

    MusikKita

---
+++ app/src/main/java/com/mdp/musikkita/Help.java
@@ -0,0 +1,14 @@
+package com.mdp.musikkita;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Help extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
